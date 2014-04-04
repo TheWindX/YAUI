c51346a34d52215b8d3f6c694286b2f7ac8c3f70
TheWindX <453588006@gmail.com>
2014-04-04 15:54:03 +0800

    testPropertyTemplate

---
+++ YAUIUser/testPropertyTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAUIUser
+{
+    using ns_YAUI;
+
+    class testPropertyTemplate : Singleton<testPropertyTemplate>
+    {
+        public testPropertyTemplate()
+        {
+            UIRoot.Instance.root.appendFromXML(
+@"
+    <rect dragAble=""true"" margin=""8"" paddingX=""4"" paddingY=""4"" shrink=""true"" layout=""horizon"">
+        <rect width=""128"" height=""32"" shrink=""false""></rect>
+        <rect></rect>
+    </rect>
+");
+        }
+    }
+}
