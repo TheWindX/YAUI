1ea04e3027ea81619c2418cb84d57b67546e9e3a
TheWindX <lxf0525.1@gmail.com>
2014-04-22 21:52:06 +0800

    minor change of testAlign

--- YAUIUser/testAlign.cs
+++ YAUIUser/testAlign.cs
@@ -13,15 +13,15 @@ namespace ns_YAUIUser
         {
             UI.Instance.fromXML(@"
 <resizer location='128, 128' size='384, 384' padding='8' editMode='dragAble'>
-    <lable text='LT' align='leftTop'></lable>
+    <lable text='leftTop' align='leftTop'></lable>
     <lable text='top' align='top'></lable>
-    <lable text='RT' align='rightTop'></lable>
+    <lable text='rightTop' align='rightTop'></lable>
     <lable text='left' align='left'></lable>
     <lable text='center' align='center'></lable>
     <lable text='right' align='right'></lable>
-    <lable text='LB' align='leftBottom'></lable>
+    <lable text='leftBottom' align='leftBottom'></lable>
     <lable text='bottom' align='bottom'></lable>
-    <lable text='RB' align='rightBottom'></lable>
+    <lable text='rightBottom' align='rightBottom'></lable>
 </resizer>
 ");
         }
